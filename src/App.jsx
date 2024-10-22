115e6104c62b2f9d61b3bf619335aabdfb0f10db
Saurabh Kumar <sauravkumar405@gmail.com>
2024-10-22 17:49:02 +0530

    fir commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Todo from './components/project2/Todo'
+
+
+function App() {
+
+  return (
+    <>
+    <Todo/>
+      
+    </>
+  )
+}
+
+export default App

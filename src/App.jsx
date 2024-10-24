0a962ef436fb28ffc776d0107e06a764837af77f
Saurabh Kumar <sauravkumar405@gmail.com>
2024-10-24 23:36:55 +0530

    renamed folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
-import Todo from './components/project2/Todo'
+import Todo from './projects/project2/Todo'
+import Timer from './projects/project1/Timer'
+import StarRating from './projects/project3/StarRating'
 import './App.css'
 
 
@@ -8,7 +10,7 @@ function App() {
 
   return (
     <>
-    <Todo/>
+
       
     </>
   )
